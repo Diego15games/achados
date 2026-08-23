3a5fee2416202f6f066057709b226354f74e4405
Diego15games <diegoviniciusgoncalvesdelirali@gmail.com>
2026-08-23 20:34:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "online",
+    message: "API Achadinhos funcionando!"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
